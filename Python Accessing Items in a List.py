092c15e12688869857944ba42bde091fc7cdf00b
BodiIrvine <bodiirvine@gmail.com>
2024-08-21 11:56:42 -0700

    exported from Skill Struck

---
+++ Python Accessing Items in a List.py
@@ -0,0 +1,12 @@
+int1 = 2
+int2 = 2
+
+addition = int1 + int2
+subtraction = int1 - int2
+multiplication = int1 * int2
+devision = int1 / int2
+
+print(int1 + int2)
+print(int1 - int2)
+print(int1 * int2)
+print(int1 / int2)
