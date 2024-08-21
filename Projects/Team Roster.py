49d767362730cfb2c1925fd689e4876c960177a2
BodiIrvine <bodiirvine@gmail.com>
2024-08-21 11:56:46 -0700

    exported from Skill Struck

---
+++ Projects/Team Roster.py
@@ -0,0 +1,14 @@
+print("Joe " + str(1))
+print("Bill " + str(2))
+print("Fred " + str(3))
+print("John " + str(4))
+print("Steve " + str(5))
+print("Carl " + str(6))
+print("Owen " + str(7))
+print("Will " + str(8))
+print("Hayden " + str(9))
+print("Seth " + str(10))
+
+string1 = "Welcome to the team! Your team number is {}"
+number = 12
+print(string1.format(number))
