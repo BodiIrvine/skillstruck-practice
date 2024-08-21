07c88d229122fdd9f2112cb682fa279a6b4d0368
BodiIrvine <bodiirvine@gmail.com>
2024-08-21 11:56:43 -0700

    exported from Skill Struck

---
+++ Projects/Mad Lips.py
@@ -0,0 +1,14 @@
+
+#here is where i sorted the input 
+noun1 = input('noun: ')
+pnoun1 = input('pnoun: ')
+noun2 = input('noun: ')
+place = input('place: ')
+adjective = input('adjective: ')
+
+#this is simply where I used the print and the rest of the story with the noun and evreything else will be enterd into the story
+print("Did you know I have a pet " + noun1 + "? "
+      "He likes to run around and play with all of the " + pnoun1 + ". "
+      "One morning, I woke up and he was wearing a " + noun2 + " for a hat! "
+      "I especially like to take him to the " + place + " "
+      "because he shows off his " + adjective + " side.")
