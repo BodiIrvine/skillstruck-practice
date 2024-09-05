fb790dcfbde1705ca085daf81fe91d311e0968a2
BodiIrvine <bodiirvine@gmail.com>
2024-09-05 11:43:24 -0700

    exported from Skill Struck

---
+++ Python 2/Adding to Lists in Python.py
@@ -0,0 +1,9 @@
+olympic_events = ['100m Sprint', 'Long Jump', 'High Jump', 'Marathon', 'Swimming']
+
+new_events = ['Surfing', 'Skateboarding', 'Climbing', 'Basketball 3x3']
+
+
+olympic_events.extend(new_events)
+
+
+print(olympic_events)
