cdae9dd3ebd55d6d6cb655af1b779a8206683969
BodiIrvine <bodiirvine@gmail.com>
2024-09-05 11:43:22 -0700

    exported from Skill Struck

---
+++ Python 2/If statement for a loop.py
@@ -0,0 +1,8 @@
+food = ["mushrooms", "broccoli", "fish"]
+for x in food:
+    if x == "mushrooms":
+        print(x + "are gross")
+    elif x == "broccoli":
+        print(x + "are gross")
+    else:
+        print(x +"are grose")
