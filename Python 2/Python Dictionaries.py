d058a083e524bab8eb98b8ff4f8b6f9d76181b19
BodiIrvine <bodiirvine@gmail.com>
2024-09-05 11:43:30 -0700

    exported from Skill Struck

---
+++ Python 2/Python Dictionaries.py
@@ -0,0 +1,14 @@
+mountains = {
+    "Timpanogos": 3582,
+    "Everest": 8848,
+    "Kilimanjaro": 5895,
+    "Vesuvius": 1281
+}
+
+print(mountains)
+
+print(mountains["Vesuvius"])
+
+mountains["Kilimanjaro"] = 6000
+
+print(mountains)
