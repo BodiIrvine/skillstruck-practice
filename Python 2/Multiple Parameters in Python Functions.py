2a445c2b45f9a011d671225c098fcadd4a4e13e0
BodiIrvine <bodiirvine@gmail.com>
2024-09-05 11:43:29 -0700

    exported from Skill Struck

---
+++ Python 2/Multiple Parameters in Python Functions.py
@@ -0,0 +1,7 @@
+def name(first, last):
+    print("The first name is " + first)
+    print("The last name is " + last)
+
+# Call the function with different parameters
+name("John", "Doe")
+name("Jane", "Smith")
